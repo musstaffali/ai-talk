36f5538678f91545bf6e3df609dc26212223a871
Mustafa <mamusstaffali@gmail.com>
2023-09-26 19:16:47 -0400

    audio to text

--- Voice_GPT2.py
+++ Voice_GPT2.py
@@ -30,13 +30,33 @@ def generate_response(prompt):
     return response["choices"][0]["text"]
 
 def speak_text(text):
-engine.say(text)
-engine runAndWait()
+    engine.say(text)
+    engine runAndWait()
 
 def main():
     while True:
         # Wait for user to say "genius"
         print("Say 'Genius' to start recording your question...")
         with sr.Microphone() as source:
-            recognizer = ai.Recognizer()
+            recognizer = sr.Recognizer()
             audio = recognizer.listen(source)
+            try: 
+                transcription = recognizer.recognizer_google(audio)
+                if transcription.lower() == "genius":
+                    # Record audio
+                    filename = "input.wav"
+                    print("Say your question...")
+                    with sr.microphone() as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit=None, time=None)
+                        with open(filename, "wb") as f:
+                            f.write(audio.get_wav_data())
+
+                    # Transcribe audio to text 
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print(f"You said: {text}")
+
+                        # Generate reponse using GPT-3
+                        reponse = generate_response
